ec3cd588da468185a0864f77ec82667d1e5077bd
shaonsani <shaon.sani.ss1@gmail.com>
2022-03-20 22:55:57 +0600

    Add GlucoseReading model to app module

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from app.models import GlucoseReading
 
-# Register your models here.
+admin.site.register(GlucoseReading)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class GlucoseReading(models.Model):
+    UNIT_1 = "mmol/L"
+    UNIT_2 = "mg/dL"
+    UNIT_CHOICES = [
+        (UNIT_1, "mmol/L"),
+        (UNIT_1, "mg/dL")
+    ]
+    reading_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    patient_uuid = models.UUIDField()
+    value = models.FloatField()
+    unit = models.CharField(choices=UNIT_CHOICES, max_length=10)
+    recorded_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.reading_uuid} | {self.recorded_at}"
